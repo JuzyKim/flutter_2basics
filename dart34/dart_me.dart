61a226281ebdd1c60c21110c81ee5ebf0c607ca4
JuzyKim <zhuldyztashbolat0@gmail.com>
2024-04-24 13:51:52 +0600

    add

---
+++ dart34/dart_me.dart
@@ -0,0 +1,23 @@
+void main() {
+  print('Hello world: !');
+  hello();
+  printName();
+  documentation();
+}
+
+/// this is hello method, do yourself
+void hello() {
+  print("Hello");
+}
+
+/// methods for printing name
+void printName() {
+  print("My name is Zhuldyz, I am from Merke");
+}
+
+/**
+* this type of comment for documentation
+*/
+void documentation() {
+  print("I am studying at JIHC College.I am second course student.");
+}
