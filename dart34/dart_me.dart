3b979f89de6274bb59b663deeec407f0adbe67c2
JuzyKim <zhuldyztashbolat0@gmail.com>
2024-04-24 13:59:29 +0600

    add

--- dart34/dart_me.dart
+++ dart34/dart_me.dart
@@ -12,12 +12,17 @@ void hello() {
 
 /// methods for printing name
 void printName() {
-  print("My name is Zhuldyz, I am from Merke");
+  print(
+      "My name is Zhuldyz, I am from Merke.I am studying at JIHC College.I am second course student.");
 }
 
 /**
 * this type of comment for documentation
 */
 void documentation() {
-  print("I am studying at JIHC College.I am second course student.");
+  print("""
+Date of Birth: April 25, 2007 
+Contact Information: madenummida@email.com | 8 705 725 9807 
+Occupation/Profession: IT Programmist 
+""");
 }
